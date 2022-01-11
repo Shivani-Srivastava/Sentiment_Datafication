2948f71776b9ae21867a2787fbf69cbf808e2d70
Shivani Srivastava <shivani141296@gmail.com>
2022-01-11 12:28:31 +0530

    Create dependency.R

---
+++ dependency.R
@@ -0,0 +1,18 @@
+if(!require("shiny")) {install.packages("shiny")}
+if(!require("tidyverse")){install.packages("tidyverse")}
+if(!require("ggplot2")){install.packages("ggplot2")}
+if(!require("DT")){install.packages("DT")}
+if(!require("tidytext")){install.packages("tidytext")}
+if(!require("dplyr")){install.packages("dplyr")}
+if(!require("plotly")){install.packages("plotly")}
+if(!require("tools")){install.packages("tools")}
+if(!require("stringr")){install.packages("stringr")}
+
+library(shiny)
+library(tidytext)
+library(dplyr)
+library(tidyverse)
+library(stringr)
+library(ggplot2)
+library(plotly)
+library(tools)
