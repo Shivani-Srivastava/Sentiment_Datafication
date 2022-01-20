e979e4c8d2bf1e4010985e0c0d38780886b2b8bf
Shivani Srivastava <shivani141296@gmail.com>
2022-01-20 14:59:05 +0530

    Update server.R

--- server.R
+++ server.R
@@ -34,6 +34,20 @@ shinyServer(function(input, output) {
         }
     })     
     
+    dataset2 <- reactive({
+      if (is.null(input$file2)) {return(NULL)}
+      else {
+        
+        if(file_ext(input$file2$datapath)=="txt"){
+          Document = readLines(input$file2$datapath)
+          return(Document)}
+        else{
+          Document = read.csv(input$file2$datapath ,header=TRUE, sep = ",", stringsAsFactors = F)
+          return(Document)
+        }
+        
+      }
+    })
     
     
     senti_df = read.csv("https://raw.githubusercontent.com/sudhir-voleti/sample-data-sets/master/sentiments.csv")
@@ -52,15 +66,31 @@ shinyServer(function(input, output) {
     
     
     a00 = reactive({calc_senti_score(dataset(), token0(), lexicon0(), afinn_senti, nrc_senti, emots0())})
+    a01 = reactive({calc_senti_score(dataset2(), token0(), lexicon0(), afinn_senti, nrc_senti, emots0())})
     
     output$Senti_Table <-renderTable({as.data.frame(a00()[1])})
-    #    if (is.null(input$file)|input$apply==0) {return(NULL)}
-    #    else{
-    #        a <- (as.data.frame(a00()[1]))
-    #        a #}
-    #)
+    
+    
+    output$ComparisonData <- renderTable(as.data.frame(a01()[1]))
+    
+    a00_dtm <- reactive({dtm_build(a00()[[1]]$senti_tooltip)})
+    a00_count <- reactive({dtm.word.count(a00_dtm())})
+    
+    a01_dtm <- reactive({dtm_build(a01()[[1]]$senti_tooltip)})
+    a01_count <- reactive({dtm.word.count(a01_dtm())})
+    
+    output$WordCloud1 <- renderPlot(
+      dtm.word.cloud(a00_count(), max.words = 100, title = "Word Cloud for Data 1")
+    )
+    
+    
+    output$WordCloud2 <- renderPlot(
+      dtm.word.cloud(a01_count(), max.words = 100, title = "Word Cloud for Data 2")
+    )
     
     output$Plot <- renderPlotly({a00()[[2]]})
+    
+    output$VersusPlot <- renderPlotly({a01()[[2]]})
 #        if (is.null(input$file)|input$apply==0) {return(NULL)}
 #        else{
             
@@ -68,5 +98,7 @@ shinyServer(function(input, output) {
    #         a #}
    # )
 
+    
+    
     
     })
