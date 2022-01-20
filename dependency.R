fc4cb07ee39019c4b5ad34ec5822e3698778674e
Shivani Srivastava <shivani141296@gmail.com>
2022-01-20 15:00:29 +0530

    Update dependency.R

--- dependency.R
+++ dependency.R
@@ -7,6 +7,9 @@ if(!require("dplyr")){install.packages("dplyr")}
 if(!require("plotly")){install.packages("plotly")}
 if(!require("tools")){install.packages("tools")}
 if(!require("stringr")){install.packages("stringr")}
+if(!require("wordcloud")){install.packages("wordcloud")}
+if(!require("tibble")){install.packages("tibble")}
+
 
 library(shiny)
 library(tidytext)
@@ -16,3 +19,5 @@ library(stringr)
 library(ggplot2)
 library(plotly)
 library(tools)
+library(wordcloud)
+library(tibble)
